876c6bdfe99f2e8da2cd9d99ca736f1e5855d155
Matheus Alexandre <mathe.system@gmail.com>
2019-07-30 20:41:15 -0300

    First commit - Example React Hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const { name } = props;
+  // Entrada, rodando, fim
+  const [stage, setStage] = useState('Entrada');
+
+  // palpites que a máquina deu
+  const [guess, setGuess] = useState(150);
+  const [guessNumber, setGuessNumber] = useState(1);
+
+  // 0 <> 300
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(300);
+
+  const startGame = () => {
+    setStage('Rodando');
+    setMin(0);
+    setMax(300);
+    setGuess(150);
+    setGuessNumber(1);
+  };
+
+  if (stage === 'Entrada') return <button onClick={startGame}>Start!!</button>;
+
+  const lessThan = () => {
+    if (guess !== 0) setGuessNumber(guessNumber + 1);
+    setMax(guess);
+    const nextGuess = parseInt((guess - min) / 2) + min;
+    setGuess(nextGuess);
+  };
+
+  const greaterThan = () => {
+    if (guess !== 0 && stage !== 'Finish!') setGuessNumber(guessNumber + 1);
+    setMin(guess);
+    const nextGuess = parseInt((max - guess) / 2) + guess;
+    setGuess(nextGuess);
+  };
+
+  const found = () => {
+    setStage('Fim!');
+  };
+
+  if (stage === 'Fim!') {
+    return <>
+      <p>Acertou no número {guess} com {guessNumber} chutes!</p>
+      <button onClick={startGame}>Iniciar novamente</button>
+    </>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <h2>O seu palpite é {guess}?</h2>
+      <span>Min: {min} | Max: {max}</span>
+      <h2>Número de palpites: {guessNumber}?</h2>
+      <button onClick={lessThan}>Menor!</button>
+      <button onClick={found}>Acertou!</button>
+      <button onClick={greaterThan}>Maior!</button>
     </div>
   );
 }
